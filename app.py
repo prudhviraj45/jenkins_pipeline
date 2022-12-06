4724114f2e5a9c3c26394d7563aa65d44ba003d6
prudhviraj45 <115442733+prudhviraj45@users.noreply.github.com>
2022-12-06 11:54:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return jsonify({"Time of Call Home Page change Pipeline_Jenkins": time.time()})
+    return jsonify({"Time of Call Home Page change abc123": time.time()})
 
 @app.route("/one")
 def one():
